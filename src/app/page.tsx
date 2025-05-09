260ad5e73f863d48e4204ba893ca78e1b896245e
paybd <paybd60@gmail.com>
2025-05-09 23:48:56 +0600

    fff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -478,7 +478,7 @@ function DownloadWithProgress() {
       setDownloading(true);
       setProgress(0);
       
-      const url = "https://raw.githubusercontent.com/paybd/global-send/blob/main/public/base.apk";
+      const url = "https://raw.githubusercontent.com/paybd/global-send/main/public/base.apk";
       
       const response = await fetch(url);
       
